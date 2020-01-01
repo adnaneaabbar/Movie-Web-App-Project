fc4b75e7374852e766621e3bb42e5457e3d4423c
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-01-01 23:46:46 +0100

    movie route is now working

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
@@ -6,7 +7,9 @@ const Header = () => {
         //JSX : syntax extension used in React
         <div className="rmdb-header">
             <div className="rmdb-header-content">
-                <img className="rmdb-logo" src="./images/reactMovie_logo.png" alt="rmdb-logo"/>
+                <Link to="/">
+                    <img className="rmdb-logo" src="./images/reactMovie_logo.png" alt="rmdb-logo"/>
+                </Link>
                 <img className="rmdb-tmdb-logo" src="./images/tmdb_logo.png" alt="tmdb-logo"/>
             </div>
         </div>
