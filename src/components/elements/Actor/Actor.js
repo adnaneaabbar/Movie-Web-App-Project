f48873b5120e12d8d820300b43ee13ec24ea7947
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-01-02 02:10:08 +0100

    Actor component finished

--- src/components/elements/Actor/Actor.js
+++ src/components/elements/Actor/Actor.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import {IMAGE_BASE_URL} from '../../../config';
+import {IMAGE_BASE_URL, POSTER_SIZE} from '../../../config';
 import './Actor.css';
 
 const Actor = (props) => {
     return (
-        <div>
-            Actor
+        <div className="rmdb-actor">
+            <img 
+                src={props.actor.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${props.actor.profile_path}` : './images/no_image.jpg'}
+                alt="actorthumb"
+            />
+            <span className="rmdb-actor-name">{props.actor.name}</span>
+            <span className="rmdb-actor-character">{props.actor.character}</span>
         </div>
     )
 }
